b6cd099d64197e8352ee15f0ea81eae8524ac821
Bach Le <lehoangbach7802@gmail.com>
2022-05-18 11:49:28 -0400

    commit tree 7907ab8aa1bbf41e250a3bd1c8ddab87a241413b

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Code your solutions in this file
+// const  gifts = [];
+
+// function writeCards(gifts, event) {
+//     const messages = [];
+//     for (let i = 0; i < gifts.length; i++) {
+//         messages.push(`Thank you, ${gifts[i]}, for the wonderful ${event} gift!`);
+//     }
+//     return gifts;
+// }
+
+// writeCards();
+
+const names = [];
+
+function writeCards(names, event) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+
+    return messages;
+}
+
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
+const number = [1,2,3,4,5,6,7,8,9,10];
+
+function countDown(number) {
+    let i = 10
+    while (i>=0) {
+        console.log(i--)
+    }
+
+    return number;
+}
